07516870017f2320b83095ca80e8f341a1e956b8
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-01-24 13:38:15 +0000

    :sparkles: Added basic about page

---
+++ src/about/about.11tydata.js
@@ -0,0 +1,44 @@
+const ContentfulApi = require("../../lib/api.js");
+const GraphQLStringBlocks = require("../../lib/graphQLStringBlocks.js");
+
+const ContentfulPerson = {
+  /*
+   * Get my info
+   */
+  get: async function () {
+    const variables = { slug: "whitep4nth3r" };
+
+    const query = `query GetPerson($slug: String!) {
+       personCollection(where: { slug: $slug }) {
+          items {
+            name
+            description
+            twitterUsername
+            gitHubUsername
+            twitchUsername
+            bioShort
+            image {
+              ${GraphQLStringBlocks.imageAsset()}
+            }
+            bioLong {
+              json
+            }
+          }
+        }
+    }`;
+
+    // Call out to the base API call
+    const response = await ContentfulApi.callContentful(query, variables);
+
+    const personCollection = response.data.personCollection.items ? response.data.personCollection.items : [];
+
+    return personCollection[0];
+  },
+};
+
+module.exports = async function () {
+  const person = await ContentfulPerson.get();
+  return {
+    person,
+  };
+};
---
+++ src/about/index.11ty.js
@@ -0,0 +1,19 @@
+const RichText = require("../_components/richText");
+const ResponsiveImage = require("../_components/responsiveImage");
+
+exports.data = {
+  layout: "base.html",
+  title: "About",
+};
+
+exports.render = function (data) {
+  const { person } = data;
+  return `
+    <h1>${person.name}</h1>
+    ${ResponsiveImage({ image: person.image })}
+    ${RichText(person.bioLong)}
+    <p>Twitter: @${person.twitterUsername}</p>
+    <p>Twitch: @${person.twitchUsername}</p>
+    <p>Github: @${person.gitHubUsername}</p>
+  `;
+};
