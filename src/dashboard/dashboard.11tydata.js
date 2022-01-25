51b66f957559d3e8db84e256d8d2e1986cb4bd22
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-01-25 16:23:10 +0000

    :sparkles: Update things???

--- src/dashboard/dashboard.11tydata.js
+++ src/dashboard/dashboard.11tydata.js
@@ -1,26 +1,26 @@
 const fetch = require("node-fetch");
 
 module.exports = async function () {
-  // const youtube = await fetch(`${process.env.DOMAIN}/api/youtube`);
-  // const youtubeData = await youtube.json();
+  const youtube = await fetch(`${process.env.DOMAIN}/api/youtube`);
+  const youtubeData = await youtube.json();
 
-  // const twitch = await fetch(`${process.env.DOMAIN}/api/twitch`);
-  // const twitchData = await twitch.json();
+  const twitch = await fetch(`${process.env.DOMAIN}/api/twitch`);
+  const twitchData = await twitch.json();
 
-  // const github = await fetch(`${process.env.DOMAIN}/api/github`);
-  // const githubData = await github.json();
+  const github = await fetch(`${process.env.DOMAIN}/api/github`);
+  const githubData = await github.json();
 
-  // const twitter = await fetch(`${process.env.DOMAIN}/api/twitter`);
-  // const twitterData = await twitter.json();
+  const twitter = await fetch(`${process.env.DOMAIN}/api/twitter`);
+  const twitterData = await twitter.json();
 
-  // const staticDashboard = {
-  //   github: githubData,
-  //   twitch: twitchData,
-  //   twitter: twitterData,
-  //   youtube: youtubeData,
-  // };
+  const staticDashboard = {
+    github: githubData,
+    twitch: twitchData,
+    twitter: twitterData,
+    youtube: youtubeData,
+  };
 
   return {
-    // staticDashboard,
+    staticDashboard,
   };
 };
