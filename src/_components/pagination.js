add21ab575076f7f9db886b82af0b5472e077063
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-04 13:52:09 +0000

    :sparkles: Pagination styles

--- src/_components/pagination.js
+++ src/_components/pagination.js
@@ -1,11 +1,19 @@
 function Pagination({ previous, next, currentPage, totalPages }) {
   return /* html */ `
-  <nav aria-labelledby="pagination">
-     <p id="pagination">Next and previous pagination</p>
-     <ol>
-      <li>${previous ? `<a href="${previous}">Previous</a>` : `Previous`}</li>
-      <li>Page ${currentPage + 1} of ${totalPages}</li>
-      <li>${next ? `<a href="${next}">Next</a>` : `Next`}</li>
+  <nav aria-labelledby="pagination" class="pagination">
+     <p id="pagination" class="pagination__description">Next and previous pagination</p>
+     <ol class="pagination__list">
+      <li class="pagination__listItem">${
+        previous
+          ? `<a href="${previous}"><span class="pagination__icon pagination__icon--prev">←</span> Previous</a>`
+          : `<span class="pagination__icon pagination__icon--prev pagination__icon--disabled">←</span> Previous`
+      }</li>
+      <li class="pagination__listItem">Page ${currentPage + 1} of ${totalPages}</li>
+      <li class="pagination__listItem">${
+        next
+          ? `<a href="${next}">Next <span class="pagination__icon pagination__icon--next">→</span></a>`
+          : `Next <span class="pagination__icon pagination__icon--next pagination__icon--disabled">→</span>`
+      }</li>
     </ol>
   </nav>`;
 }
