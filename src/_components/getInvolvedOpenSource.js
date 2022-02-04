c5d91612afc7ea3b398368b9c3280a1fe2d54972
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-04 13:12:19 +0000

    :sparkles: Style header

--- src/_components/getInvolvedOpenSource.js
+++ src/_components/getInvolvedOpenSource.js
@@ -3,7 +3,7 @@ const SeeAllCta = require("./seeAllCta");
 function GetInvolvedOpenSource() {
   return /*html*/ `
     <div class="homeCard">
-      <h3 class="homeCard__title">Get involved with open source</h3>
+      <h1 class="homeCard__title">Get involved with open source</h1>
       <div class="homeCard__excerpt">
         <p>TO DO TO DO</p>
       </div>
