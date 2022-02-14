ef8cf9992bd0945d6ef286ca1b813d9002b0a144
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-14 13:54:40 +0000

    :sparkles: Style uses

--- src/uses/index.11ty.js
+++ src/uses/index.11ty.js
@@ -3,11 +3,15 @@ const OpenGraph = require("../../lib/openGraph");
 
 const pageTitle = "Things whitep4nth3r uses for coding, streaming, and everything else";
 
+var md = require("markdown-it")({
+  html: true,
+});
+
 exports.data = {
   layout: "base.html",
   title: pageTitle,
   metaDescription:
-    "I receive a lot of questions on stream about my setup and what I use. So here's a (probably very incomplete) list!.",
+    "I receive a lot of questions on stream about my setup and what I use. So here's a (probably very incomplete) list!",
   openGraphImageUrl: OpenGraph.generateImageUrl({ title: pageTitle }),
   openGraphImageAlt: OpenGraph.generateImageAlt(pageTitle),
   openGraphImageWidth: OpenGraph.imageWidth,
@@ -34,20 +38,36 @@ exports.render = function (data) {
             ${AboutTableOfContents({ onUses: true, categories })}
           </aside>
 
+              <p class="uses__intro">I receive a lot of questions on stream about my setup and what I use. Here's a (probably very incomplete) list to make it easier to share.</p>
 
               ${categories
                 .map(
                   (cat) => `
                 <section id="${cat}">
-                  <h2>${cat}</h2>
+                  <div class="uses__sectionHeader">
+                    <h2 class="uses__sectionHeaderTitle">${cat}</h2>
+                  </div>
+                  <ol class="uses__list">
                   ${things[cat]
                     .map(
                       (thing) =>
                         `
-                      <p>${thing.name}</p>
+                      <li class="uses__listItem">
+                        <div>
+                          <h3 class="uses__listItemName uses__listItemName--small">${thing.name}</h3>
+                          <img class="uses__listItemImg" src="${thing.image.url}?w=200" alt="${
+                          thing.image.description
+                        } height="${thing.image.height}" width="${thing.image.width}" />
+                        </div>
+                        <div>
+                          <h3 class="uses__listItemName uses__listItemName--large">${thing.name}</h3>
+                          <div class="uses__listItemDesc">${md.render(thing.description)}</div>
+                        </div>
+                      </li>
                     `,
                     )
                     .join("")}
+                  </ol>
                 </section>`,
                 )
                 .join("")}
