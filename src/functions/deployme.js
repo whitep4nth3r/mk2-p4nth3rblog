0d4ca5e65392514185e56e258ae7aa85f411b9a4
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-15 12:58:17 +0000

    :art: Update deployme function

--- src/functions/deployme.js
+++ src/functions/deployme.js
@@ -2,13 +2,13 @@ const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
   if (event.queryStringParameters.secret === process.env.DEPLOY_ME_SECRET) {
-    const response = await fetch(process.env.DEPLOY_ME_URL, {
+    const response = fetch(process.env.DEPLOY_ME_URL, {
       method: "POST",
     });
 
     return {
       statusCode: 200,
-      body: "Site has been deployed! Have a great day!",
+      body: "Your site is now deploying! Have a great day!",
     };
   } else {
     return {
