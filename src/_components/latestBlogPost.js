f3423e95b4901f00becc07b8341ca99da0d5a46f
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-16 13:41:22 +0000

    :sparkles: Add post meta to home page latest blog post

--- src/_components/latestBlogPost.js
+++ src/_components/latestBlogPost.js
@@ -1,5 +1,8 @@
 const TopicsButton = require("./topicsButton");
 const SeeAllCta = require("./seeAllCta");
+const StopwatchIcon = require("../_components/svg/stopwatchIcon");
+const CalendarIcon = require("../_components/svg/calendarIcon");
+const DateUtils = require("../../lib/dateUtils");
 
 var md = require("markdown-it")({
   html: true,
@@ -11,6 +14,13 @@ function LatestBlogPost({ post }) {
       <a href="/blog/${post.slug}/" class="homeCard__linkForTitle" id="post-${post.sys.id}">
         <h3 class="homeCard__title">${post.title}</h3>
       </a>
+
+      <p class="postCard__meta">
+        <span class="postCard__metaIcon">${CalendarIcon()}</span>
+        <span class="postCard__metaText">${DateUtils.formatDateForDisplay(post.date)}</span>
+        <span class="postCard__metaIcon">${StopwatchIcon()}</span>
+        <span class="postCard__metaText">${post.readingTime} min read</span>
+      </p>
 
       <div class="homeCard__excerpt">
         ${md.render(post.excerpt)}
