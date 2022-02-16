1f1bb9523b09596e9e8e1a4e60b49bf7c9e0eb9c
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-16 13:19:19 +0000

    :bug: Fix stopwatch

--- src/_components/svg/stopwatchIcon.js
+++ src/_components/svg/stopwatchIcon.js
@@ -6,6 +6,7 @@ function StopwatchIcon() {
       height="24"
       width="24"
       role="img"
+      fill="none"
       aria-label="Stopwatch"
     >
       <path d="M12 21C16.4183 21 20 17.4183 20 13C20 8.58172 16.4183 5 12 5C7.58172 5 4 8.58172 4 13C4 17.4183 7.58172 21 12 21Z" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
