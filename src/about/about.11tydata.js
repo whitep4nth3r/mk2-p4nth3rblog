b308e8b969769c1b1a304ab199c51537986304f3
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-17 13:26:04 +0000

    :art: Move all data collection to lib directory

--- src/about/about.11tydata.js
+++ src/about/about.11tydata.js
@@ -1,94 +1,13 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
 
-const ContentfulApi = require("../../lib/contentfulApi.js");
-const GraphQLStringBlocks = require("../../lib/graphQLStringBlocks.js");
-
-function sortItemsByDateAsc(a, b) {
-  const a_timestamp = Date.parse(a.date);
-  const a_date = new Date(a_timestamp);
-
-  const b_timestamp = Date.parse(b.date);
-  const b_date = new Date(b_timestamp);
-
-  return a_date - b_date;
-}
+const ContentfulEvents = require("../../lib/contentfulEvents.js");
+const ContentfulPerson = require("../../lib/contentfulPerson");
+const DateUtils = require("../../lib/dateUtils");
 
 function removeTwitchStreamsWhenOnVacation(events, startTime, endTime) {
   return events.filter((ev) => ev.date < startTime || ev.date > endTime);
 }
-
-const ContentfulEvents = {
-  /*
-   * Get events
-   */
-  getAll: async function ({ future }) {
-    // Calculate date_ASC for future events, or date_DESC for past events
-    const order = future ? "date_ASC" : "date_DESC";
-
-    // Generate today's date
-    const date = new Date();
-
-    // And format it to an ISO String
-    const formattedDate = date.toISOString();
-
-    // Decide on the date filter to pass in as a string
-    const dateFilter = future ? "date_gt" : "date_lt";
-
-    // Construct variables object to send with the HTTP POST request
-    const variables = { date: formattedDate, order };
-
-    // Build the query
-    const query = `query GetEvents($date: DateTime!, $order: [EventOrder]!) {
-      eventCollection(where: {${dateFilter}: $date}, order: $order) {
-        items {
-          sys {
-            id
-          }
-          date
-          name
-          link
-        }
-      }
-    }`;
-
-    const response = await ContentfulApi.callContentful(query, variables);
-
-    const eventCollection = response.data.eventCollection.items ? response.data.eventCollection.items : [];
-
-    return eventCollection;
-  },
-};
-
-const ContentfulPerson = {
-  /*
-   * Get my info
-   */
-  get: async function () {
-    const variables = { slug: "whitep4nth3r" };
-
-    const query = `query GetPerson($slug: String!) {
-       personCollection(where: { slug: $slug }) {
-          items {
-            imageBio {
-              ${GraphQLStringBlocks.imageAsset()}
-            }
-            bioShort
-            bioSpeaker
-            bioLong {
-              json
-            }
-          }
-        }
-    }`;
-
-    const response = await ContentfulApi.callContentful(query, variables);
-
-    const personCollection = response.data.personCollection.items ? response.data.personCollection.items : [];
-
-    return personCollection[0];
-  },
-};
 
 module.exports = async function () {
   const person = await ContentfulPerson.get();
@@ -122,7 +41,7 @@ module.exports = async function () {
       : twitchEvents;
 
   const allEvents = dbEvents.concat(filteredTwitchEvents);
-  const sortedEvents = allEvents.sort(sortItemsByDateAsc);
+  const sortedEvents = allEvents.sort(DateUtils.sortItemsByDateAsc);
 
   return {
     person,
