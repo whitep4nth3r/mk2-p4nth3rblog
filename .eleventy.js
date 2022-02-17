ae779277daa6e3ce4719c2f4a92f88e5244c84a0
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-17 13:45:53 +0000

    :art: Fix pass through errors

--- .eleventy.js
+++ .eleventy.js
@@ -7,12 +7,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("./src/_css/main.css.map");
   eleventyConfig.ignores.add("./src/_scripts");
 
-  eleventyConfig.addWatchTarget("./src/_css");
-  eleventyConfig.addPassthroughCopy({ "./src/_css": "./src/css" });
-
-  eleventyConfig.addPassthroughCopy({ "./src/_fonts": "./src/fonts" });
+  eleventyConfig.addWatchTarget("./src/css");
+  eleventyConfig.addPassthroughCopy({ "./src/_css": "/css" });
+  eleventyConfig.addPassthroughCopy({ "./src/_fonts": "/fonts" });
   eleventyConfig.addPassthroughCopy({ "./src/_public": "/" });
-
   eleventyConfig.addPassthroughCopy({ "./src/_search/app.js": "/js/app_search.js" });
   eleventyConfig.addPassthroughCopy({ "./node_modules/@github/time-elements/dist/index.js": "js/time_elements.js" });
 
