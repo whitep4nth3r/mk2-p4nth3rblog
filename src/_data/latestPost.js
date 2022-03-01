6f1b0fd7cd8c564c3b36806370a43d788a460126
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-03-01 15:50:50 +0000

    :sparkles: Add missing file

---
+++ src/_data/latestPost.js
@@ -0,0 +1,35 @@
+const ContentfulApi = require("../../lib/contentfulApi");
+const GraphQLStringBlocks = require("../../lib/graphQLStringBlocks");
+
+async function getLatestBlogPost() {
+  const variables = { limit: 1 };
+
+  const query = `query GetLatestBlogPost($limit: Int!) {
+    blogPostCollection(limit: $limit, order: date_DESC) {
+      items {
+        sys {
+          id
+        }
+        date
+        updatedDate
+        title
+        slug
+        excerpt
+        readingTime
+        ${GraphQLStringBlocks.topicsCollection()}
+        ${GraphQLStringBlocks.featuredImage()}
+        ${GraphQLStringBlocks.authorBasic()}
+      }
+    }
+  }`;
+
+  const response = await ContentfulApi.callContentful(query, variables);
+  const posts = response.data.blogPostCollection.items ? response.data.blogPostCollection.items : [];
+  return posts[0];
+}
+
+module.exports = async function () {
+  const latestPost = await getLatestBlogPost();
+
+  return { post: latestPost };
+};
