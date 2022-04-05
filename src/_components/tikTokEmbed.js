f5fa84f2341c1e81cbe3c44ed15a5605664cf388
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-04-05 09:59:03 +0100

    :bulb: clean up

--- src/_components/tikTokEmbed.js
+++ src/_components/tikTokEmbed.js
@@ -1,7 +1,5 @@
 function TikTokEmbed({ url, title }) {
-  // the embed URL needs to be in a <p> tag for the youtube plugin!
-
-  // TO DO
+  // the embed URL needs to be in a <p> tag for the tiktok plugin!
   return /* html */ `
     <p class="videoEmbed__cta">${title}</p>
     <div class="videoEmbed__tiktok">
