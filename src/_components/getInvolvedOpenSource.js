178563c122eb9add61ee249978dfdae6dc18d655
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-07-29 14:02:41 +0100

    :sparkles: WIP home page redesign and refactor of heading to semibold

--- src/_components/getInvolvedOpenSource.js
+++ src/_components/getInvolvedOpenSource.js
@@ -4,9 +4,9 @@ const DiscordIcon = require("./svg/discordIcon");
 function GetInvolvedOpenSource() {
   return /*html*/ `
     <div class="homeCard">
-      <h1 class="homeCard__title">Join the community</h1>
+      <h2 class="homeCard__title">Join the community</h2>
       <div class="homeCard__excerpt">
-        <p>Join a growing community of developers who are building stuff, learning things, and helping each other grow through the power of <span class="colorHighlight"><strong>open source software</strong></span>.</p>
+        <p>Join a growing community of developers who are building stuff, learning things, and helping each other grow through the power of open source software.</p>
       </div>
       <div>
       <a href="/discord" rel="nofollow noreferrer" target="_blank" class="homeCard__button">
---
+++ src/_components/homeAbout.js
@@ -0,0 +1,50 @@
+function homeAbout({ person }) {
+  console.log(person.imageBio);
+  const imageUrl = `${person.imageBio.url}?w=200&q=75`;
+
+  console.log(imageUrl);
+
+  return /* html */ `
+  <div class="homeAbout">
+
+
+      
+
+
+
+        <h1 class="homeAbout__name">
+          <span>Salma</span>
+          <span>Alam-Naylor</span>
+        </h1>
+        <h2 class="homeAbout__subtitle">Staff Developer Experience Engineer at Netlify</h2>
+
+        <p class="homeAbout_minibio">I'm a <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, <a href="/blog/">writer</a>, and <a href="https://twitch.tv/whitep4nth3r">live streamer</a>. I help developers build stuff, learn things, and love what they do through writing and live streaming about tech and building open source projects.</p>
+
+
+
+<!--
+    <div class="homeAbout__banner__imgContainer">
+        <picture>
+          <source
+            type="image/avif"
+            srcset="${imageUrl}&fm=avif" 
+          />
+          <source
+            type="image/webp"
+            srcset="${imageUrl}&fm=webp" 
+          />
+          <img
+          src="${imageUrl}"
+          alt="${person.imageBio.description}" 
+          height="${person.imageBio.height}" 
+          width="${person.imageBio.width}"
+          />
+        </picture>
+      </div>
+
+      -->
+
+  </div>`;
+}
+
+module.exports = homeAbout;
