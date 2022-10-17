bb41b26c3d5010bff01c6cbed2869bb69a5ec9da
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-17 14:36:09 +0100

    :art: Small homepage updates

--- src/_components/homeAbout.js
+++ src/_components/homeAbout.js
@@ -12,7 +12,7 @@ function homeAbout({ person }) {
         <span>Alam-Naylor</span>
       </h1>
     </div>
-    <h2 class="homeAbout__subtitle">Staff Developer Experience Engineer at <a href="http://www.netlify.com?utm_campaign=devex-san&utm_source=whitep4nth3r-blog&utm_content=netlify-home" target="_blank">Netlify</a></h2>
+    <h2 class="homeAbout__subtitle">Staff Developer Experience Engineer at <a href="https://ntl.fyi/3vA7krR" target="_blank">Netlify</a></h2>
     <p class="homeAbout_minibio">I'm a <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, <a href="/blog/">writer</a>, and <a href="https://twitch.tv/whitep4nth3r">live streamer</a>. I help developers build stuff, learn things, and love what they do through blog posts, tutorial videos, live streaming about tech and building open source projects.</p>
   </div>`;
 }
