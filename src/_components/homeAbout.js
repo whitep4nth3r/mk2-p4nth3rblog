8870867ee050886bc925f8f9ef47d866ce16173a
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-21 10:43:04 +0100

    :art: Small change to home page

--- src/_components/homeAbout.js
+++ src/_components/homeAbout.js
@@ -13,7 +13,7 @@ function homeAbout({ person }) {
       </h1>
     </div>
     <h2 class="homeAbout__subtitle">Staff Developer Experience Engineer at <a href="https://ntl.fyi/3vA7krR" target="_blank">Netlify</a></h2>
-    <p class="homeAbout_minibio">I'm a <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, <a href="/blog/">writer</a>, and <a href="https://twitch.tv/whitep4nth3r">live streamer</a>. I help developers build stuff, learn things, and love what they do through blog posts, tutorial videos, live streaming about tech and building open source projects.</p>
+    <p class="homeAbout_minibio">I'm a <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, <a href="/blog/">writer</a>, and <a href="https://twitch.tv/whitep4nth3r">live streamer</a>. I help developers build stuff, learn things, and love what they do through blog posts, tutorial videos, live coding and building open source projects.</p>
   </div>`;
 }
 
