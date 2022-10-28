9e9e2c31452880ad0abedc965e2dfa71d63f5e15
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-28 13:55:09 +0100

    :speech_balloon: Update wording

--- src/_components/homeAbout.js
+++ src/_components/homeAbout.js
@@ -13,7 +13,7 @@ function homeAbout({ person }) {
       </h1>
     </div>
     <h2 class="homeAbout__subtitle">✨ I write code for your entertainment ✨</h2>
-    <p class="homeAbout_minibio">I'm a <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, <a href="/blog/">writer</a>, and <a href="https://twitch.tv/whitep4nth3r">live streamer</a>. I help developers build 🔥 cool stuff 🔥 with blog posts, tutorial videos, live coding and open source code. I work at <a href="https://ntl.fyi/3vA7krR" target="_blank">Netlify</a>.</p>
+    <p class="homeAbout_minibio">I'm a <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, <a href="/blog/">writer</a>, and <a href="https://twitch.tv/whitep4nth3r">live streamer</a>. I help developers build 🔥 cool stuff 🔥 with blog posts, tutorial videos, live coding and open source projects. I work at <a href="https://ntl.fyi/3vA7krR" target="_blank">Netlify</a>.</p>
   </div>`;
 }
 
