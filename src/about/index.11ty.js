38a1152c74b551fdf580a040ac3422fc9bdcbb2f
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-10-28 14:00:02 +0100

    :art: Small title changes

--- src/about/index.11ty.js
+++ src/about/index.11ty.js
@@ -80,7 +80,7 @@ exports.render = function (data) {
           
           <section id="events" class="about__events">
             <div class="about__eventsHeader">
-              <h2 class="about__eventsHeaderTitle">events <span class="colorHighlight">and talks</span></h2>
+              <h2 class="about__eventsHeaderTitle">Events and <span class="colorHighlight">live streams</span></h2>
             </div>
             
               <ol class="about__eventsList">
