c8e513de74175f414a47dd801cb7caecb26be858
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-11-30 14:48:53 +0000

    :lipstick: Update design of isLive on next twitch stream

--- src/_components/nextTwitchStream.js
+++ src/_components/nextTwitchStream.js
@@ -6,11 +6,64 @@ function noStreamIndicator(text) {
   return /*html*/ `<span class="nextTwitchStream__noStream">${text}</span>`;
 }
 
-function NextTwitchStream({ stream, link, isLive }) {
-  const heading = isLive ? "⚡️ I'm live on Twitch right now! Join the stream." : `Next Twitch stream`;
-  const buttonText = isLive ? "Watch live" : "View stream details";
-  const buttonLink = isLive ? "https://twitch.tv/whitep4nth3r" : link;
+function notLive({ link, image, stream }) {
+  return /* html */ `
+    ${ResponsiveImage({ image })}
+    <h3 class="nextTwitchStream__title">
+        Next Twitch stream
+      ${stream.canceled_until ? noStreamIndicator("cancelled") : ""}
+    </h3>
+    <a href="${link}" target="_blank" rel="nofollow noreferrer" class="nextTwitchStream__link">
+      <p class="nextTwitchStream__meta">
+        <span class="nextTwitchStream__metaIcon">${ClockIcon()}</span>
+        <span class="nextTwitchStream__metaInfo" data-time="${stream.start_time}"></span>
+      </p>
+    </a>
+    <p class="nextTwitchStream__streamInfo">
+        Join the stream whilst I build weird websites, roast your code, and chat about the tech industry.
+    </p>
+    <div class="homeCard__ctaRow">
+      <a href="${link}"
+      class="nextTwitchStream__cta"
+      target="_blank" title="Watch live on Twitch" rel="nofollow noreferrer">
+        <span>
+          ${TwitchIcon()}
+        </span>
+        <span>
+          View stream details
+        </span>
+      </a>
+    </div>`;
+}
 
+function live() {
+  return /* html */ `
+    <div class="nextTwitchStream__player">
+      <iframe
+        src="https://player.twitch.tv/?channel=whitep4nth3r&parent=${process.env.TWITCH_HOST}"
+        height="100%"
+        width="100%"
+        muted
+        autoplay
+        allowfullscreen>
+      </iframe>
+    </div>
+    <div class="homeCard__ctaRow">
+      <a href="https://twitch.tv/whitep4nth3r"
+        class="nextTwitchStream__cta"
+        target="_blank" title="Watch live on Twitch" rel="nofollow noreferrer">
+        <span>
+          ${TwitchIcon()}
+        </span>
+        <span>
+          Watch live
+        </span>
+      </a>
+    </div>
+`;
+}
+
+function NextTwitchStream({ stream, link, isLive }) {
   const image = {
     title: "twitch_offline_banner",
     description:
@@ -23,61 +76,9 @@ function NextTwitchStream({ stream, link, isLive }) {
 
   return /*html*/ `
     <div class="nextTwitchStream">
-        ${!isLive ? ResponsiveImage({ image }) : ""}
-        <h3 class="nextTwitchStream__title">
-          ${heading}
-          ${stream.canceled_until ? noStreamIndicator("cancelled") : ""}
-        </h3>
-
-      ${
-        isLive
-          ? `<div class="nextTwitchStream__player">
-                <iframe
-                  src="https://player.twitch.tv/?channel=whitep4nth3r&parent=${process.env.TWITCH_HOST}"
-                  height="100%"
-                  width="100%"
-                  muted
-                  autoplay
-                  allowfullscreen>
-                </iframe>
-              </div>`
-          : ""
-      }
-              
-      ${
-        !isLive
-          ? `
-          <a href="${buttonLink}" target="_blank" rel="nofollow noreferrer" class="nextTwitchStream__link">
-            <p class="nextTwitchStream__meta">
-              <span class="nextTwitchStream__metaIcon">${ClockIcon()}</span>
-              <span class="nextTwitchStream__metaInfo" data-time="${stream.start_time}"></span>
-            </p>
-          </a>`
-          : ""
-      }
-
-        ${
-          !isLive
-            ? `<p class="nextTwitchStream__streamInfo">
-              Join the stream whilst I build weird websites, roast your code, and chat about the tech industry.
-            </p>`
-            : ""
-        }
-
-        <div class="homeCard__ctaRow">
-          <a href="${buttonLink}"
-          class="nextTwitchStream__cta"
-          target="_blank" title="Watch live on Twitch" rel="nofollow noreferrer">
-            <span>
-              ${TwitchIcon()}
-            </span>
-            <span>
-              ${buttonText}
-            </span>
-          </a>
-        </div>
-      </div>
-    `;
+      ${isLive ? live() : notLive({ link, image, stream })}
+    </div>
+  `;
 }
 
 module.exports = NextTwitchStream;
