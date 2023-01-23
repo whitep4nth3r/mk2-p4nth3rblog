e18b00df7934a949727d8a6a24827541269a2042
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2023-01-23 17:16:17 +0000

    :art: Switch around the order of social links

--- src/_components/aboutSocialLinks.js
+++ src/_components/aboutSocialLinks.js
@@ -11,13 +11,13 @@ function AboutSocialLinks() {
   return /*html*/ `
   <ul class="about__socialLinks">
     <li>
+      <a href="https://twitch.tv/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${TwitchIcon()} Twitch</a>
+    </li>
+    <li>
       <a href="/discord" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${DiscordIcon()} Discord</a>
     </li>
-    <li>
+        <li>
       <a href="https://github.com/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${GithubIcon()} Github</a>
-    </li>
-    <li>
-      <a href="https://twitch.tv/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${TwitchIcon()} Twitch</a>
     </li>
     <li>
       <a href="https://indieweb.social/@whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${MastodonIcon()} Mastodon</a>
@@ -27,9 +27,6 @@ function AboutSocialLinks() {
     </li>
     <li>
       <a href="https://youtube.com/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${YoutubeSquare()} YouTube</a>
-    </li>
-    <li>
-      <a href="https://dev.to/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${DevIcon()} DEV</a>
     </li>
     <li>
       <a href="/feed.xml" class="about__socialLinksItem" target="_blank">${FeedIcon()} Feed</a>
