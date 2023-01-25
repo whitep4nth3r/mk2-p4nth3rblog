5becc5b77d15a9c3c5c8ab8837221f2ef1db88cc
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2023-01-25 09:12:49 +0000

    :fire: Move around some more social links

--- src/_components/aboutSocialLinks.js
+++ src/_components/aboutSocialLinks.js
@@ -3,9 +3,7 @@ const GithubIcon = require("./svg/githubIcon");
 const TwitchIcon = require("./svg/twitchIcon");
 const TwitterIcon = require("./svg/twitterIcon");
 const YoutubeSquare = require("./svg/youtubeSquare");
-const DevIcon = require("./svg/devIcon");
 const FeedIcon = require("./svg/feedIcon");
-const MastodonIcon = require("./svg/mastodonIcon");
 
 function AboutSocialLinks() {
   return /*html*/ `
@@ -14,19 +12,16 @@ function AboutSocialLinks() {
       <a href="https://twitch.tv/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${TwitchIcon()} Twitch</a>
     </li>
     <li>
-      <a href="/discord" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${DiscordIcon()} Discord</a>
-    </li>
-        <li>
       <a href="https://github.com/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${GithubIcon()} Github</a>
     </li>
     <li>
-      <a href="https://indieweb.social/@whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${MastodonIcon()} Mastodon</a>
+      <a href="https://youtube.com/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${YoutubeSquare()} YouTube</a>
     </li>
     <li>
       <a href="https://twitter.com/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${TwitterIcon()} Twitter</a>
     </li>
     <li>
-      <a href="https://youtube.com/whitep4nth3r" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${YoutubeSquare()} YouTube</a>
+      <a href="/discord" class="about__socialLinksItem" target="_blank" rel="nofollow noreferrer">${DiscordIcon()} Discord</a>
     </li>
     <li>
       <a href="/feed.xml" class="about__socialLinksItem" target="_blank">${FeedIcon()} Feed</a>
