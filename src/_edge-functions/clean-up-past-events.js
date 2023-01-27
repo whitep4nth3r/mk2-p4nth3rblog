df8c6b6b0e08c195701272f010290a4e619780b2
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2023-01-27 12:13:08 +0000

    :zap: Use EF to remove past events on about page if three hours have passed and I forgot to redeploy my site

---
+++ src/_edge-functions/clean-up-past-events.js
@@ -0,0 +1,26 @@
+import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
+
+
+// this is a temporary solution to remove "past" events on a visit to the page
+// if I have forgotten to rebuild my static site to update it!
+
+export default async (request, context) => {
+  const response = await context.next();
+
+  return new HTMLRewriter()
+    .on("[event-date]", {
+      element(element) {
+        const dateString = element.getAttribute("event-date");
+        const date = new Date(dateString);
+        const now = new Date();
+
+        // add three hours to avoid removing events that are in progress
+        const cutOff = now.setTime(now.getTime() + 3 * 60 * 60 * 1000);
+
+        if (cutOff > date) {
+          element.remove();
+        }
+      },
+    })
+    .transform(response);
+};
