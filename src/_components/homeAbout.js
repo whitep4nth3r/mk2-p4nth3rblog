6f437bc24af69e899a78da40ca3b780138710dd2
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2023-03-02 12:29:44 +0000

    :lipstick: Add AtomicMarker font and do some work on the home page

--- src/_components/homeAbout.js
+++ src/_components/homeAbout.js
@@ -1,14 +1,12 @@
-
-
 function homeAbout() {
   return /* html */ `
   <div class="homeAbout">
     <h1 class="homeAbout__name">
-      <span>Salma</span>
-      <span>Alam-Naylor</span>
+      <span class="homeAbout__nameFirst">Salma</span>
+      <span class="homeAbout__nameLast">Alam-Naylor</span>
     </h1>
-    <h2 class="homeAbout__subtitle">✨ I write code ✨ for your entertainment</h2>
-    <p class="homeAbout_minibio">I'm a <a href="https://twitch.tv/whitep4nth3r">live streamer</a>, <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, and <a href="/blog/">writer</a>. I help developers build 🔥 cool stuff 🔥 with blog posts, tutorial videos, live coding and open source projects. I work at <a href="https://ntl.fyi/3vA7krR" target="_blank">Netlify</a>.</p>
+    <h2 class="homeAbout__subtitle">I write code for your entertainment.</h2>
+    <p class="homeAbout_minibio">I'm a <a href="https://twitch.tv/whitep4nth3r">live streamer</a>, <a href="https://github.com/whitep4nth3r" target="_blank">software engineer</a>, and <a href="/blog/">writer</a>. I help developers build cool stuff with blog posts, tutorial videos, live coding and open source projects. I work at <a href="https://ntl.fyi/3vA7krR" target="_blank">Netlify</a>.</p>
   </div>`;
 }
 
