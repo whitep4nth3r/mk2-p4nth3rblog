ff25912dab851e7b6189d39d5d153579220a198f
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2023-05-03 14:46:03 +0100

    :lipstick: Some more CSS

--- src/_components/blogSidebarAuthor.js
+++ src/_components/blogSidebarAuthor.js
@@ -1,7 +1,5 @@
 const BioImage = require("./bioImage");
-const TwitterIcon = require("./svg/twitterIcon");
-const TwitchIcon = require("./svg/twitchIcon");
-const GitHubIcon = require("./svg/githubIcon");
+const SocialLinks = require("./socialLinks");
 
 function BlogSidebarAuthor({ author }) {
   return /*html*/ `
@@ -9,11 +7,10 @@ function BlogSidebarAuthor({ author }) {
     <div class="blogSidebarAuthor__imgContainer">
       ${BioImage({ image: author.imageBio })}
     </div>
-    <a href="/about/" class="blogSidebarAuthor__cta">About Salma <span aria-hidden="true">→</span></a>
+    <a href="/about/" class="blogSidebarAuthor__cta">About Salma</a>
+
     <div class="blogSidebarAuthor_social">
-      <a href="https://twitch.tv/whitep4nth3r" class="blogSidebarAuthor_socialLink" aria-label="Twitch channel">${TwitchIcon()}</a>
-      <a href="https://github.com/whitep4nth3r" class="blogSidebarAuthor_socialLink" aria-label="GitHub profile">${GitHubIcon()}</a>
-      <a href="https://twitter.com/whitep4nth3r" class="blogSidebarAuthor_socialLink" aria-label="Twitter profile">${TwitterIcon()}</a>
+      ${SocialLinks()}
     </div>
   </div>
   `;
