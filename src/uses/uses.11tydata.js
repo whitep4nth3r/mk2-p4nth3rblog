c27565b2c5f8360e7d694ac277c0aaf8673a00d0
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-08-10 15:08:00 +0100

    Each thing I use now only has one category

--- src/uses/uses.11tydata.js
+++ src/uses/uses.11tydata.js
@@ -7,7 +7,7 @@ module.exports = async function () {
   const things = {};
 
   for (let i = 0; i < categories.length; i++) {
-    things[categories[i]] = allThings.filter((thing) => thing.categories.includes(categories[i]));
+    things[categories[i]] = allThings.filter((thing) => thing.category === categories[i]);
   }
 
   return {
