ac280af395fe55ae64e4d91b982b488bc5a9f7d0
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-01-16 17:46:13 +0000

    Reinstate sponsorships page

--- src/_archive/sponsorships/sponsorships.11tydata.js
+++
@@ -1,7 +0,0 @@
-// const ContentfulSponsorshipPage = require("../../lib/contentfulSponsorshipPage");
-
-// module.exports = async function () {
-//   const content = await ContentfulSponsorshipPage.get({ slug: "sponsorships" });
-
-//   return { content };
-// };
