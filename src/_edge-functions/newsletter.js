11036e05509034f6a57eb7c723d2581db572af9a
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-01-16 20:00:46 +0000

    Simplify newsletter EF

--- src/_edge-functions/newsletter.js
+++ src/_edge-functions/newsletter.js
@@ -1,10 +1,7 @@
 import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
 
 export default async (request, context) => {
-  const BASE_URL = "https://api.buttondown.email";
-  const ENDPOINT = "/v1/subscribers";
-
-  const subscribers = await fetch(`${BASE_URL}${ENDPOINT}`, {
+  const subscribers = await fetch(`https://api.buttondown.email/v1/subscribers`, {
     headers: {
       Authorization: `Token ${Deno.env.get("BUTTONDOWN_API_KEY")}`,
     },
