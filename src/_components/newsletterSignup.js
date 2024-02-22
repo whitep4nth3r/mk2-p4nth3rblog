118e3a6d397545071210015c3ca4472729854a3f
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-02-22 11:22:18 +0000

    Move newsletter data to static build

--- src/_components/newsletterSignup.js
+++ src/_components/newsletterSignup.js
@@ -1,9 +1,9 @@
-function NewsletterSignup({ removeMargin = false }) {
+function NewsletterSignup({ removeMargin = false, subscribers }) {
   const modifier = removeMargin ? " newsletterSignup--noMargin" : "";
   return /* html */ `  
      <div class="newsletterSignup${modifier}">
         <h2 class="newsletterSignup__title">Want <span>weird stuff</span> in your inbox?</h2>
-        <p class="newsletterSignup__para">Join <span data-wwwh-subs>100+</span> subscribers in the Weird Wide Web Hole to find no answers to questions you didn't know you had.</p>
+        <p class="newsletterSignup__para">Join ${subscribers}+ subscribers in the Weird Wide Web Hole to find no answers to questions you didn't know you had.</p>
         <a href="https://buttondown.email/weirdwidewebhole" class="newsletterSignup__submit" target="_blank">Subscribe</a>
     </div>
       `;
