46c12e13de71694ac1b00209ddebef64bcefeba9
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-05-09 14:42:39 +0100

    Change text on newsletter subscribe card

--- src/_components/newsletterSignup.js
+++ src/_components/newsletterSignup.js
@@ -2,7 +2,7 @@ function NewsletterSignup({ removeMargin = false, subscribers }) {
   const modifier = removeMargin ? " newsletterSignup--noMargin" : "";
   return /* html */ `  
      <div class="newsletterSignup${modifier}">
-        <h2 class="newsletterSignup__title">Want <span>weird stuff</span> in your inbox?</h2>
+        <h2 class="newsletterSignup__title">Like <span>weird</span> newsletters?</h2>
         <p class="newsletterSignup__para">Join ${subscribers} subscribers in the Weird Wide Web Hole to find no answers to questions you didn't know you had.</p>
         <a href="https://buttondown.email/weirdwidewebhole" class="newsletterSignup__submit" target="_blank">Subscribe</a>
     </div>
