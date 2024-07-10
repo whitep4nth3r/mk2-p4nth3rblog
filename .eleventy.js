4cbf624593ffe404567105f4864f085c2dd851ad
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-07-10 09:22:47 +0100

    Remove custom fonts, substitute brush font elements with SVG

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addWatchTarget("./src/css");
   eleventyConfig.addPassthroughCopy({ "./src/_css/": "/css/" });
-  eleventyConfig.addPassthroughCopy({ "./src/_fonts": "/fonts" });
   eleventyConfig.addPassthroughCopy({ "./src/_public": "/" });
   eleventyConfig.addPassthroughCopy({
     "./src/_client_scripts/app_search.js": "/js/app_search.js",
