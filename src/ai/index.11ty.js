582d1efa060352f1f95b2ec0f440a48c8098420e
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-10-30 11:01:44 +0000

    Add AI page content

--- src/ai/index.11ty.js
+++ src/ai/index.11ty.js
@@ -14,11 +14,30 @@ exports.data = {
   includeInSitemap: true,
 };
 
-exports.render = function (data) {
+exports.render = function () {
   return /* html */ `
-    <section class="ai">
-      <h1 class="page__headerTitle">AI usage and thoughts</h1>
-    
-    </section>
+   <div class="ai__content">  
+      <h1 class="ai__title">AI Usage</h1>
+      <p class="ai__text">Everything I create on the internet — including this website and my newsletter — is created, built and written by me using my own thoughts, ideas, and experience.</p>
+      <p class="ai__text">I may use LLMs and AI lightly as tools during the content creation process to help with things like summarising large transcripts or articles that I use as source material. Sometimes I use tools like GitHub Copilot to help me write code (but it's more fun and a better learning experience to write the code by hand).</p>
+      <p class="ai__text">This website is my playground and my exploration space, and I like to painstakingly handcraft it myself.</p>
+      <p class="ai__text">I have a strong dislike for AI-generated art and music. Whilst it may be a quick way to generate royalty-free assets for your project, it is devoid of imagination, creativity and the human spirit.</p>
+      <p class="ai__text">This page is my <a href="https://www.bydamo.la/p/ai-manifesto" target="_blank">AI manifesto</a>.</p>
+      <div class="ai__cta">
+        <div class="newsletterSignup">
+          <h2 class="newsletterSignup__title">
+            Want
+            <span>weird stuff</span>
+            in your inbox?
+          </h2>
+          <p class="newsletterSignup__para">
+            Enter the Weird Wide Web Hole to find no answers to questions you didn't know you had.
+          </p>
+          <a href="https://buttondown.email/weirdwidewebhole" class="newsletterSignup__submit" target="_blank">
+            Subscribe
+          </a>
+        </div>
+      </div>
+    </div>
   `;
 };
