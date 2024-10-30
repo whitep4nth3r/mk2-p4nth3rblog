52181f5cd5a145a316cecc3c516083bdb405a74b
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-10-30 11:04:01 +0000

    Update words

--- src/ai/index.11ty.js
+++ src/ai/index.11ty.js
@@ -18,7 +18,7 @@ exports.render = function () {
   return /* html */ `
    <div class="ai__content">  
       <h1 class="ai__title">AI Usage</h1>
-      <p class="ai__text">Everything I create on the internet — including this website and my newsletter — is created, built and written by me using my own thoughts, ideas, and experience.</p>
+      <p class="ai__text">Everything I publish on the internet — including this website and my newsletter — is created, built and written by me using my own thoughts, ideas, and experience.</p>
       <p class="ai__text">I may use LLMs and AI lightly as tools during the content creation process to help with things like summarising large transcripts or articles that I use as source material. Sometimes I use tools like GitHub Copilot to help me write code (but it's more fun and a better learning experience to write the code by hand).</p>
       <p class="ai__text">This website is my playground and my exploration space, and I like to painstakingly handcraft it myself.</p>
       <p class="ai__text">I have a strong dislike for AI-generated art and music. Whilst it may be a quick way to generate royalty-free assets for your project, it is devoid of imagination, creativity and the human spirit.</p>
