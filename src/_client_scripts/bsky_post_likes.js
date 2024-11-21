2c3b0622539769a1333400c3d8d65c70b46883e9
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-11-21 13:47:22 +0000

    Prepare bsky likes code for expansion at some point

--- src/_client_scripts/bsky_post_likes.js
+++ src/_client_scripts/bsky_post_likes.js
@@ -1,5 +1,7 @@
 const bskyPostId = document.querySelector("#bsky_post_id").dataset.bskyPostId;
 const likesContainer = document.querySelector("[data-bsky-likes]");
+const myDid = "did:plc:qcxqtc2yzznbaazu7egncqqx";
+const getLikesURL = `https://public.api.bsky.app/xrpc/app.bsky.feed.getLikes?uri=`;
 
 function drawLikes(likes) {
   for (const like of likes) {
@@ -14,12 +16,10 @@ function drawLikes(likes) {
 }
 
 if (bskyPostId !== null) {
-  const myDid = "did:plc:qcxqtc2yzznbaazu7egncqqx";
   const postUri = `at://${myDid}/app.bsky.feed.post/${bskyPostId}`;
 
-  const bskyPostLikes = await fetch("https://public.api.bsky.app/xrpc/app.bsky.feed.getLikes?uri=" + postUri);
+  const bskyPostLikes = await fetch(getLikesURL + postUri);
   const data = await bskyPostLikes.json();
-  console.log(data.likes);
 
   if (data.likes.length > 0) {
     drawLikes(data.likes);
