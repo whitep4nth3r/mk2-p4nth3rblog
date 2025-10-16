d594f389a344f6c14125eae333c8c3b56f75e34d
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2025-10-16 14:20:24 +0100

    Remove old import

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const embedYouTube = require("eleventy-plugin-youtube-embed");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("./src/_sass");
