176b7b911d2fcd47558e3a4f19cfdf5e7fc6adc7
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2026-01-05 15:35:58 +0000

    Only enable gsap scrolltrigger on 1080px+

--- src/_client_scripts/home.js
+++ src/_client_scripts/home.js
@@ -1,21 +1,38 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
-  gsap.to(".hero__name__inner", {
-    x: "100%",
-    ease: "none",
-    scrollTrigger: {
-      trigger: "html",
-      start: "top top",
-      end: "bottom top",
-      scrub: true,
-    },
-  });
+
+  const mediaQuery = window.matchMedia("(min-width: 1080px)");
+  let heroTween;
+
+  function handleMediaQuery(e) {
+    if (e.matches) {
+      if (!heroTween) {
+        heroTween = gsap.to(".hero__name__inner", {
+          x: "100%",
+          ease: "none",
+          scrollTrigger: {
+            trigger: "html",
+            start: "top top",
+            end: "bottom top",
+            scrub: true,
+          },
+        });
+      }
+    } else {
+      if (heroTween) {
+        heroTween.scrollTrigger.kill();
+        heroTween.kill();
+        heroTween = null;
+      }
+    }
+  }
+
+  handleMediaQuery(mediaQuery);
+  mediaQuery.addEventListener("change", handleMediaQuery);
 
   const headerHeight = document.querySelector("header").offsetHeight;
   const hero = document.querySelector("[data-hero]");
   hero.style.height = `calc(100svh - ${headerHeight}px)`;
-
-  
 
   window.addEventListener("resize", () => {
     const updatedHeaderHeight = document.querySelector("header").offsetHeight;
