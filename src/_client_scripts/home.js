14e7865608f2003698cc930940c18333a5c43cba
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2026-01-07 22:04:41 +0000

    Fix typo

--- src/_client_scripts/home.js
+++ src/_client_scripts/home.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
           scrollTrigger: {
             trigger: "html",
             start: "top top",
-            end: "+=500",
+            end: "bottom top",
             scrub: true,
           },
         });
